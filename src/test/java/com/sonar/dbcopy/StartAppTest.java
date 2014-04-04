4ff15a3a41be2d6d7ec4e5d5d218f987255c111c
stephenbroyer <stephen.broyer@sonarsource.com>
2014-04-04 14:25:12 +0200

    1.0 Fix SonarQube Issues (Insufficient branch coverage by unit tests)

--- src/test/java/com/sonar/dbcopy/StartAppTest.java
+++ src/test/java/com/sonar/dbcopy/StartAppTest.java
@@ -13,13 +13,16 @@ import org.junit.After;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class StartAppTest {
 
-  private Connection connectionSourceV1, connectionSourceV2, connectionDestV1, connectiondestV3;
+  private Connection connectionSourceV1, connectionSourceV2, connectionDestV1, connectiondestV3, connectionSourceToption, connectionDestToption;
 
   @Test
   public void testMain() throws Exception {
@@ -47,6 +50,52 @@ public class StartAppTest {
     };
     StartApp startApp = new StartApp();
     startApp.main(args);
+  }
+
+  @Test
+  public void testMainWithTableOption() throws Exception {
+
+    Utils utils = new Utils();
+    connectionSourceToption = utils.makeFilledH2("StartAppTest_Source_T_option_DB", true);
+    utils.addContentInThirdTable(connectionSourceToption, 1);
+    utils.addContentInThirdTable(connectionSourceToption, 2);
+
+    connectionDestToption = utils.makeEmptyH2("StartAppTest_Destination_T_option_DB", true);
+    utils.addContentInThirdTable(connectionDestToption, 2);
+
+
+    String[] args = {
+      "-driverSrc", "org.h2.Driver",
+      "-urlSrc", "jdbc:h2:mem:StartAppTest_Source_T_option_DB;DB_CLOSE_ON_EXIT=-1;",
+      "-userSrc", "sonar",
+      "-pwdSrc", "sonar",
+      "-driverDest", "org.h2.Driver",
+      "-urlDest", "jdbc:h2:mem:StartAppTest_Destination_T_option_DB;DB_CLOSE_ON_EXIT=-1;",
+      "-userDest", "sonar",
+      "-pwdDest", "sonar",
+      "-T", "table_for_test"
+    };
+    StartApp startApp = new StartApp();
+    startApp.main(args);
+
+    Statement statementDestToption = connectionDestToption.createStatement();
+    ResultSet resultSet = statementDestToption.executeQuery("SELECT * FROM schema_migrations");
+    // THE TEST WORKS IF THE TABLE schema_migrations DON'T HAVE A ROW WITH version=1 WHICH WOULD COME FROM SOURCE
+    while (resultSet.next()) {
+      assertEquals(2, resultSet.getInt(1));
+    }
+
+    // AND THE TABLE  table_for_test MUST BE FULL AND HAVE A RESULTSET WHITH DATA
+    resultSet = statementDestToption.executeQuery("SELECT * FROM table_for_test");
+    assertThat(resultSet).isNotNull();
+    while (resultSet.next()) {
+      assertThat(resultSet.getInt(1)).isNotNull();
+    }
+
+    Closer closer = new Closer("startAppTestMain");
+    closer.closeConnection(connectionSourceToption);
+    closer.closeConnection(connectionDestToption);
+
   }
 
   @Test
@@ -84,6 +133,8 @@ public class StartAppTest {
   @After
   public void tearDown() {
     Closer closer = new Closer("startAppTesttearDown");
+    closer.closeConnection(connectionSourceToption);
+    closer.closeConnection(connectionDestToption);
     closer.closeConnection(connectionSourceV1);
     closer.closeConnection(connectionDestV1);
     closer.closeConnection(connectionSourceV2);
