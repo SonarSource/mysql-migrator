6b007764f7066cbd7998bb743d8ddade17f0b1a8
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2017-12-20 17:30:32 +0100

    Actually scan projects and check that issues are copied to destination SQ

--- it/src/test/java/com/sonar/dbcopy/its/DbCopyIntegrationTest.java
+++ it/src/test/java/com/sonar/dbcopy/its/DbCopyIntegrationTest.java
@@ -6,14 +6,22 @@
 package com.sonar.dbcopy.its;
 
 import com.sonar.orchestrator.Orchestrator;
+import com.sonar.orchestrator.build.MavenBuild;
 import com.sonar.orchestrator.config.Configuration;
 import org.apache.commons.lang.StringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.sonarqube.ws.Issues;
+import org.sonarqube.ws.client.HttpConnector;
+import org.sonarqube.ws.client.WsClient;
+import org.sonarqube.ws.client.WsClientFactories;
+import org.sonarqube.ws.client.issue.SearchWsRequest;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
+import java.util.Collections;
 
 import static org.fest.assertions.Assertions.assertThat;
 import static org.fest.assertions.Fail.fail;
@@ -39,14 +47,18 @@ public class DbCopyIntegrationTest {
         .setProperty("orchestrator.configUrl", getSystemPropertyOrFail(ORCHESTRATOR_PROPERTIES_SOURCE))
         .addEnvVariables()
         .build()
-    ).build();
+    )
+      .addPlugin("java")
+      .build();
     destinationOrchestrator = Orchestrator.builder(
       Configuration.builder()
         .addSystemProperties()
         .setProperty("orchestrator.configUrl", getSystemPropertyOrFail(ORCHESTRATOR_PROPERTIES_DESTINATION))
         .addEnvVariables()
         .build()
-    ).build();
+    )
+      .addPlugin("java")
+      .build();
   }
 
   @After
@@ -57,16 +69,62 @@ public class DbCopyIntegrationTest {
 
   @Test
   public void shouldCopyDatabase() throws Exception {
+
     // Start source SQ
     sourceOrchestrator.start();
-    // TODO Perform 2 analyses to have some history
-    // sourceOrchestrator.executeBuildQuietly(MavenBuild.create(...));
-    // sourceOrchestrator.executeBuildQuietly(MavenBuild.create(...));
+
+    WsClient sourceWsClient = WsClientFactories.getDefault().newClient(
+      HttpConnector.newBuilder()
+        .url(sourceOrchestrator.getServer().getUrl())
+        .build());
+
+    assertThat(sourceWsClient
+      .issues()
+      .search(new SearchWsRequest())
+      .getTotal()).isZero();
+
+    // First analysis
+    sourceOrchestrator.executeBuildQuietly(
+      MavenBuild.create()
+        .setPom(new File("projects/java-sample/1/pom.xml"))
+        .setCleanPackageSonarGoals());
+
+    long sourceInitialTotal = sourceWsClient
+      .issues()
+      .search(new SearchWsRequest())
+      .getTotal();
+
+    // Second analysis, should close one additional issue
+    sourceOrchestrator.executeBuildQuietly(
+      MavenBuild.create()
+        .setPom(new File("projects/java-sample/2/pom.xml"))
+        .setCleanPackageSonarGoals());
+
+    long sourceFinalTotal = sourceWsClient
+      .issues()
+      .search(new SearchWsRequest())
+      .getTotal();
+
+    // Check that issues exist
+    assertThat(sourceFinalTotal).isGreaterThan(sourceInitialTotal);
+
     // Stop source SQ
     sourceOrchestrator.stop();
 
+
     // Start destination SQ
     destinationOrchestrator.start();
+
+    // Check no issues in destination
+    WsClient destinationWsClient = WsClientFactories.getDefault().newClient(
+      HttpConnector.newBuilder()
+        .url(destinationOrchestrator.getServer().getUrl())
+        .build());
+    assertThat(destinationWsClient
+      .issues()
+      .search(new SearchWsRequest())
+      .getTotal()).isZero();
+
     // And stop it immediately
     destinationOrchestrator.stop();
 
@@ -99,10 +157,23 @@ public class DbCopyIntegrationTest {
         .setProperty("orchestrator.keepDatabase", "true")
         .addEnvVariables()
         .build()
-    ).build();
+    )
+      .addPlugin("java")
+      .build();
     destinationOrchestrator.start();
-    // TODO Check data has been copied
-    // destinationOrchestrator.getServer().newHttpCall("").execute();
+
+    // Check one issue in destination
+    destinationWsClient = WsClientFactories.getDefault().newClient(
+      HttpConnector.newBuilder()
+        .url(destinationOrchestrator.getServer().getUrl())
+        .build());
+    long destinationFinalTotal = destinationWsClient
+      .issues()
+      .search(new SearchWsRequest())
+      .getTotal();
+
+    // Check that issues exist
+    assertThat(destinationFinalTotal).isEqualTo(sourceFinalTotal);
   }
 
   private static String getSystemPropertyOrFail(String orchestratorPropertiesSource) {
