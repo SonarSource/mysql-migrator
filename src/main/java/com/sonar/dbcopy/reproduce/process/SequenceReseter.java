fb39f00ae67855988e6b9bdbf8ec7db788712198
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2018-03-20 13:44:39 +0100

    SDBC-45 Add some traces to figure out DB copy sequence reset problem

--- src/main/java/com/sonar/dbcopy/reproduce/process/SequenceReseter.java
+++ src/main/java/com/sonar/dbcopy/reproduce/process/SequenceReseter.java
@@ -45,17 +45,22 @@ public class SequenceReseter {
 
       resultSetDest = metaDest.getPrimaryKeys(null, null, tableNameWithGoodCase);
 
+      System.out.println("Resetting sequences for table " + tableNameWithGoodCase);
+
       // IF REULTSET IS NOT "beforeFirst" THAT MEANS THERE IS A PRIMARY KEY
       if (resultSetDest.isBeforeFirst() && hasIdColumn(resultSetDest)) {
         closer.closeResultSet(resultSetDest);
         long idMaxPlusOne = CharacteristicsRelatedToEditor.getIdMaxPlusOne(connectionDest, tableName);
+        System.out.println("Got ID max +1 = " + idMaxPlusOne);
         sqlRequestToReset = CharacteristicsRelatedToEditor.makeAlterSequencesRequest(metaDest, tableName, idMaxPlusOne);
 
         // THEN RESET SEQUENCE REQUEST IS EXECUTED
         statementDest = connectionDest.createStatement();
         if (destinationIsOracle) {
+          System.out.println("Dropping sequence on Oracle");
           statementDest.execute(CharacteristicsRelatedToEditor.makeDropSequenceRequest(tableName));
         }
+        System.out.println("Will execute " + sqlRequestToReset);
         statementDest.execute(sqlRequestToReset);
       }
     } catch (SQLException e) {
@@ -70,8 +75,11 @@ public class SequenceReseter {
   private static boolean hasIdColumn(ResultSet resultSetDest) {
     try {
       resultSetDest.findColumn("id");
+      System.out.println("Found id column");
       return true;
     } catch(SQLException noSuchColumn) {
+      noSuchColumn.printStackTrace(System.err);
+      System.out.println("No id column");
       return false;
     }
   }
