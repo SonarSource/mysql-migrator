040452258f97a4c535a273c264eac002daf54cf7
Jean-Baptiste Lievremont <jeanbaptiste.lievremont@sonarsource.com>
2019-03-07 11:11:51 +0100

    Use latest travis-utils to get Artifactory URL from environment

--- travis.sh
+++ travis.sh
@@ -4,10 +4,11 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir  -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v51 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v55 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
 installTravisTools
+. installJDK8
 . installMaven35
 regular_mvn_build_deploy_analyze
