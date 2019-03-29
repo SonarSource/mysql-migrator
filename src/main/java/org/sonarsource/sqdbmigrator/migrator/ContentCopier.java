20f9258edff898e2425cb26a1e7b4ef16574d5c3
Janos Gyerik <janos.gyerik@sonarsource.com>
2019-03-29 15:58:04 +0100

    Add content copier

---
+++ src/main/java/org/sonarsource/sqdbmigrator/migrator/ContentCopier.java
@@ -0,0 +1,129 @@
+/*
+ * SonarQube MySQL Database Migrator
+ * Copyright (C) 2019-2019 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sqdbmigrator.migrator;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.stream.IntStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonarsource.sqdbmigrator.migrator.Migrator.MigrationException;
+
+public class ContentCopier {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ContentCopier.class);
+
+  private static final int DEFAULT_BATCH_SIZE = 5000;
+
+  void execute(Database source, Database target, TableListProvider tableListProvider) {
+    execute(source, target, tableListProvider, DEFAULT_BATCH_SIZE);
+  }
+
+  void execute(Database source, Database target, TableListProvider tableListProvider, int batchSize) {
+    tableListProvider.get(source).forEach(tableName -> {
+      LOG.info("copying table {} ...", tableName);
+
+      try {
+        target.truncateTable(tableName);
+
+        boolean tableHasIdColumn = source.tableHasIdColumn(tableName);
+
+        if (tableHasIdColumn) {
+          target.setIdentityInsert(tableName, true);
+        }
+
+        copyTable(source, target, tableName, batchSize);
+
+        if (tableHasIdColumn) {
+          target.setIdentityInsert(tableName, false);
+          resetSequence(target, tableName);
+        }
+
+        ensureRowCountsMatch(source, target, tableName);
+      } catch (SQLException e) {
+        throw new MigrationException("Error while copying rows of table '%s': %s", tableName, e.getMessage());
+      }
+    });
+  }
+
+  private void copyTable(Database source, Database target, String tableName, int batchSize) throws SQLException {
+    String selectSql = String.format("select * from %s", tableName);
+    try (Statement preparedStatement = source.getConnection().createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+      ResultSet rs = preparedStatement.executeQuery(selectSql)) {
+
+      int columnCount = rs.getMetaData().getColumnCount();
+
+      String insertSql = String.format("insert into %s values (%s)", tableName, formatPlaceholders(columnCount));
+      int count = 0;
+      try (PreparedStatement insertStatement = target.getConnection().prepareStatement(insertSql)) {
+        while (rs.next()) {
+          copyColumns(rs, insertStatement);
+          insertStatement.addBatch();
+
+          count++;
+          if (count % batchSize == 0) {
+            insertStatement.executeBatch();
+            target.getConnection().commit();
+            count = 0;
+          }
+        }
+
+        if (count > 0) {
+          insertStatement.executeBatch();
+          target.getConnection().commit();
+        }
+      }
+    }
+  }
+
+  private void copyColumns(ResultSet rs, PreparedStatement insertStatement) throws SQLException {
+    int columnCount = rs.getMetaData().getColumnCount();
+    for (int index = 1; index <= columnCount; index++) {
+      insertStatement.setObject(index, rs.getObject(index));
+    }
+  }
+
+  private String formatPlaceholders(int count) {
+    StringBuilder sb = new StringBuilder(count * 2 - 1);
+    sb.append("?");
+    IntStream.range(1, count).forEach(i -> sb.append(",?"));
+    return sb.toString();
+  }
+
+  private void resetSequence(Database database, String tableName) {
+    try {
+      long maxIdPlusOne = 1 + database.selectMaxId(tableName);
+      database.resetSequence(tableName, maxIdPlusOne);
+    } catch (SQLException e) {
+      throw new MigrationException("Could not reset sequence for table %s: %s", tableName, e.getMessage());
+    }
+  }
+
+  private void ensureRowCountsMatch(Database source, Database target, String tableName) throws SQLException {
+    long rowCountInSource = source.countRows(tableName);
+    long rowCountInTarget = target.countRows(tableName);
+    if (rowCountInSource != rowCountInTarget) {
+      throw new MigrationException("Row counts don't match in source and target: %s != %s",
+        tableName, rowCountInSource, rowCountInTarget);
+    }
+  }
+}
---
+++ src/test/java/org/sonarsource/sqdbmigrator/migrator/ContentCopierTest.java
@@ -0,0 +1,187 @@
+/*
+ * SonarQube MySQL Database Migrator
+ * Copyright (C) 2019-2019 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sqdbmigrator.migrator;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import java.math.BigInteger;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.sonarsource.sqdbmigrator.migrator.DatabaseTester.newTester;
+
+@RunWith(DataProviderRunner.class)
+public class ContentCopierTest {
+
+  @Rule
+  public final DatabaseTester sourceTester = newTester();
+
+  @Rule
+  public final DatabaseTester targetTester = newTester();
+
+  private final ContentCopier underTest = new ContentCopier();
+
+  @Test
+  @UseDataProvider("batchSizesAround5")
+  public void copy_all_rows_regardless_of_batchSize_boundaries(int batchSize) throws SQLException {
+    String tableName = "foo";
+    String createTableSql = String.format("create table %s (name varchar)", tableName);
+
+    sourceTester.createTable(createTableSql)
+      .addRow(tableName, Collections.singletonList("foo1"))
+      .addRow(tableName, Collections.singletonList("foo2"))
+      .addRow(tableName, Collections.singletonList("foo3"))
+      .addRow(tableName, Collections.singletonList("foo4"))
+      .addRow(tableName, Collections.singletonList("foo5"));
+
+    targetTester.createTable(createTableSql);
+
+    underTest.execute(sourceTester.getDatabase(), targetTester.getDatabase(), newTableListProvider(tableName), batchSize);
+
+    assertThat(sourceTester.getDatabase().countRows(tableName))
+      .isEqualTo(5)
+      .isEqualTo(targetTester.getDatabase().countRows(tableName));
+
+    String selectNamesSql = String.format("select name from %s", tableName);
+    assertThat(sourceTester.queryForStrings(selectNamesSql))
+      .isEqualTo(Arrays.asList("foo1", "foo2", "foo3", "foo4", "foo5"))
+      .isEqualTo(targetTester.queryForStrings(selectNamesSql));
+  }
+
+  @DataProvider
+  public static Object[][] batchSizesAround5() {
+    return new Object[][]{
+      {4},
+      {5},
+      {6},
+    };
+  }
+
+  @Test
+  public void copy_all_rows_with_all_supported_column_types() throws SQLException {
+    // supported types = the unique types that appear in schema-h2 table definitions file in SonarQube
+
+    String tableName = "t1";
+    String createTableSql = String.format(
+      "create table %s (\n" +
+        "  c_bigint BIGINT NOT NULL,\n" +
+        "  c_binary BINARY NOT NULL,\n" +
+        "  c_blob BLOB NOT NULL,\n" +
+        "  c_boolean BOOLEAN NOT NULL,\n" +
+        "  c_clob CLOB NOT NULL,\n" +
+        "  c_double DOUBLE NOT NULL,\n" +
+        "  c_int INT NOT NULL,\n" +
+        "  c_integer INTEGER NOT NULL,\n" +
+        "  c_timestamp TIMESTAMP NOT NULL,\n" +
+        "  c_tinyint TINYINT NOT NULL,\n" +
+        "  c_varchar VARCHAR(100) NOT NULL\n" +
+        ")",
+      tableName);
+
+    Date d1 = new Date();
+    Date d2 = new Date();
+
+    byte[] bytes1 = "foo".getBytes();
+    byte[] bytes2 = "bar".getBytes();
+
+    sourceTester.createTable(createTableSql)
+      .addRow(tableName, Arrays.asList(BigInteger.TEN, bytes1, bytes1, true, "foo clob", 1.23D, 12, 34, d1, 1, "foo"))
+      .addRow(tableName, Arrays.asList(11L, bytes2, bytes2, true, "bar clob", 1.23D, 12, 34, d2, 2, "bar"));
+
+    targetTester.createTable(createTableSql);
+
+    underTest.execute(sourceTester.getDatabase(), targetTester.getDatabase(), newTableListProvider(tableName));
+
+    assertThat(sourceTester.getDatabase().countRows(tableName))
+      .isEqualTo(2)
+      .isEqualTo(targetTester.getDatabase().countRows(tableName));
+
+    String selectClobsSql = String.format("select c_clob from %s", tableName);
+    assertThat(sourceTester.queryForStrings(selectClobsSql))
+      .isEqualTo(Arrays.asList("foo clob", "bar clob"))
+      .isEqualTo(targetTester.queryForStrings(selectClobsSql));
+
+    String selectTimestampsSql = String.format("select c_timestamp from %s", tableName);
+    assertThat(sourceTester.queryForObjects(selectTimestampsSql))
+      .extracting(o -> ((Timestamp) o).getTime())
+      .isEqualTo(Arrays.asList(d1.getTime(), d2.getTime()));
+    assertThat(targetTester.queryForObjects(selectTimestampsSql))
+      .extracting(o -> ((Timestamp) o).getTime())
+      .isEqualTo(Arrays.asList(d1.getTime(), d2.getTime()));
+
+    String selectBlobSql = String.format("select c_blob from %s", tableName);
+    List<byte[]> sourceBytes = sourceTester.queryForBytes(selectBlobSql);
+    assertThat(sourceBytes).hasSize(2);
+    assertThat(sourceBytes.get(0)).isEqualTo(bytes1);
+    assertThat(sourceBytes.get(1)).isEqualTo(bytes2);
+
+    List<byte[]> targetBytes = targetTester.queryForBytes(selectBlobSql);
+    assertThat(targetBytes).hasSize(2);
+    assertThat(targetBytes.get(0)).isEqualTo(bytes1);
+    assertThat(targetBytes.get(1)).isEqualTo(bytes2);
+  }
+
+  @Test
+  public void update_sequence_in_target() throws SQLException {
+    String tableName = "t1";
+    String createTableSql = String.format(
+      "create table %s (\"ID\" INTEGER NOT NULL GENERATED BY DEFAULT AS IDENTITY (START WITH 1, INCREMENT BY 1)," +
+        "  \"NAME\" VARCHAR(128) NOT NULL\n" +
+        ")",
+      tableName);
+
+    sourceTester.createTable(createTableSql)
+      .addRow(tableName, singletonList("name"), singletonList("foo"))
+      .addRow(tableName, singletonList("name"), singletonList("bar"))
+      .addRow(tableName, singletonList("name"), singletonList("baz"));
+
+    targetTester.createTable(createTableSql);
+
+    underTest.execute(sourceTester.getDatabase(), targetTester.getDatabase(), newTableListProvider(tableName));
+
+    targetTester
+      .addRow(tableName, singletonList("name"), singletonList("foo2"))
+      .addRow(tableName, singletonList("name"), singletonList("bar2"));
+
+    assertThat(targetTester.getDatabase().countRows(tableName)).isEqualTo(5);
+
+    String selectMaxIdSql = String.format("select max(id) from %s", tableName);
+    assertThat(targetTester.queryForInt(selectMaxIdSql)).isEqualTo(5);
+  }
+
+  private TableListProvider newTableListProvider(String tableName) {
+    return new TableListProvider() {
+      @Override
+      public List<String> get(Database database) {
+        return singletonList(tableName);
+      }
+    };
+  }
+}
