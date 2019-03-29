bd8b233f4fbc6e870efde66b8af379e2bec5785f
Janos Gyerik <janos.gyerik@sonarsource.com>
2019-03-29 17:54:48 +0100

    Add pre-migration checks

---
+++ src/main/java/org/sonarsource/sqdbmigrator/migrator/before/PreMigrationChecks.java
@@ -0,0 +1,52 @@
+/*
+ * SonarQube MySQL Database Migrator
+ * Copyright (C) 2019-2019 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sqdbmigrator.migrator.before;
+
+import org.sonarsource.sqdbmigrator.migrator.Database;
+import org.sonarsource.sqdbmigrator.migrator.TableListProvider;
+
+public class PreMigrationChecks {
+
+  private final VersionValidator versionValidator;
+  private final TableListValidator tableListValidator;
+
+  public PreMigrationChecks(VersionValidator versionValidator, TableListValidator tableListValidator) {
+    this.versionValidator = versionValidator;
+    this.tableListValidator = tableListValidator;
+  }
+
+  public void execute(Database source, Database target, TableListProvider tableListProvider) {
+    versionValidator.execute(source, target);
+
+    tableListValidator.execute(source, target, tableListProvider);
+
+    // TODO See issue #4: Fail if target has projects, issues, or unexpected users
+
+    // TODO See issue #4: Fail if source doesn't have projects, issues, or unexpected users
+
+    // TODO See issue #4: Fail if duplicate values are found in projects.kee of the source (print duplicates)
+  }
+
+  static class PreMigrationException extends RuntimeException {
+    PreMigrationException(String format, Object... args) {
+      super(String.format(format, args));
+    }
+  }
+}
---
+++ src/test/java/org/sonarsource/sqdbmigrator/migrator/before/PreMigrationChecksTest.java
@@ -0,0 +1,67 @@
+/*
+ * SonarQube MySQL Database Migrator
+ * Copyright (C) 2019-2019 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonarsource.sqdbmigrator.migrator.before;
+
+import org.junit.Test;
+import org.sonarsource.sqdbmigrator.migrator.Database;
+import org.sonarsource.sqdbmigrator.migrator.TableListProvider;
+import org.sonarsource.sqdbmigrator.migrator.before.PreMigrationChecks.PreMigrationException;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+public class PreMigrationChecksTest {
+
+  @Test
+  public void execute_all_validators() {
+    Database source = mock(Database.class);
+    Database target = mock(Database.class);
+    TableListProvider tableListProvider = mock(TableListProvider.class);
+
+    VersionValidator versionValidator = mock(VersionValidator.class);
+    TableListValidator tableListValidator = mock(TableListValidator.class);
+
+    new PreMigrationChecks(versionValidator, tableListValidator).execute(source, target, tableListProvider);
+    versionValidator.execute(source, target);
+    tableListValidator.execute(source, target, tableListProvider);
+  }
+
+  @Test
+  public void do_not_reach_table_list_validation_if_version_validation_failed() {
+    Database source = mock(Database.class);
+    Database target = mock(Database.class);
+    TableListProvider tableListProvider = mock(TableListProvider.class);
+
+    VersionValidator failingVersionValidator = mock(VersionValidator.class);
+    doThrow(new PreMigrationException("something went wrong")).when(failingVersionValidator).execute(source, target);
+
+    TableListValidator tableListValidator = mock(TableListValidator.class);
+
+    try {
+      new PreMigrationChecks(failingVersionValidator, tableListValidator).execute(source, target, tableListProvider);
+      fail("expected to throw");
+    } catch (PreMigrationException ignored) {
+      // nothing to do
+    }
+    verifyZeroInteractions(tableListValidator);
+  }
+}
