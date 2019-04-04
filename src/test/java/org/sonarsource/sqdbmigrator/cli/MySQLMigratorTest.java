f94a4f05efb11b64b1f46dc43973971d5352139d
Janos Gyerik <janos.gyerik@sonarsource.com>
2019-04-04 18:00:39 +0200

    Add tests running main cli

--- src/test/java/org/sonarsource/sqdbmigrator/cli/MySQLMigratorTest.java
+++ src/test/java/org/sonarsource/sqdbmigrator/cli/MySQLMigratorTest.java
@@ -27,6 +27,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import org.junit.contrib.java.lang.system.SystemErrRule;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -63,11 +65,17 @@ public class MySQLMigratorTest {
   @Rule
   public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
+  @Rule
+  public final SystemErrRule systemErrRule = new SystemErrRule().enableLog();
+
+  @Rule
+  public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
   @Test
   public void exit_with_error_when_args_empty() {
-    underTest.run(new String[0]);
-    verify(system2).printlnErr("Missing required options: -source, -target");
-    verify(system2).exit(1);
+    exit.checkAssertionAfterwards(() -> assertThat(systemErrRule.getLog()).isEqualTo("Missing required options: -source, -target\n"));
+    exit.expectSystemExitWithStatus(1);
+    realRun();
   }
 
   @Test
@@ -80,15 +88,15 @@ public class MySQLMigratorTest {
 
   @DataProvider
   public static Object[][] argsWithMissingRequiredParams() {
-    return new Object[][]{
-      {new String[]{"-source", "path"}, "Missing required option: -target"},
-      {new String[]{"-target", "path"}, "Missing required option: -source"},
+    return new Object[][] {
+      {new String[] {"-source", "path"}, "Missing required option: -target"},
+      {new String[] {"-target", "path"}, "Missing required option: -source"},
     };
   }
 
   @Test
   public void exit_with_error_when_unexpected_args_present() {
-    underTest.run(new String[]{"unexpected"});
+    underTest.run(new String[] {"unexpected"});
     verify(system2).printlnErr("Unexpected arguments: unexpected");
     verify(system2).exit(1);
   }
@@ -96,14 +104,14 @@ public class MySQLMigratorTest {
   @Test
   @UseDataProvider("validOptionNamesRequiringParameter")
   public void exit_with_error_when_option_parameter_missing(String name) {
-    underTest.run(new String[]{name});
+    underTest.run(new String[] {name});
     verify(system2).printlnErr("Option " + name + " requires a parameter");
     verify(system2).exit(1);
   }
 
   @DataProvider
   public static Object[][] validOptionNamesRequiringParameter() {
-    return new Object[][]{
+    return new Object[][] {
       {"-source"},
       {"-target"},
     };
@@ -119,21 +127,21 @@ public class MySQLMigratorTest {
 
   @DataProvider
   public static Object[][] argsIncludingHelp() {
-    return new Object[][]{
-      {new String[]{"-help"}},
-      {new String[]{"foo", "bar", "-help"}},
-      {new String[]{"-foo", "bar", "-help"}},
-      {new String[]{"-foo", "-bar", "-help"}},
-      {new String[]{"-source", "path", "-help"}},
-      {new String[]{"-target", "path", "-help"}},
-      {new String[]{"-source", "path", "-target", "path", "-help"}},
+    return new Object[][] {
+      {new String[] {"-help"}},
+      {new String[] {"foo", "bar", "-help"}},
+      {new String[] {"-foo", "bar", "-help"}},
+      {new String[] {"-foo", "-bar", "-help"}},
+      {new String[] {"-source", "path", "-help"}},
+      {new String[] {"-target", "path", "-help"}},
+      {new String[] {"-source", "path", "-target", "path", "-help"}},
     };
   }
 
   @Test
   public void exit_with_error_when_source_config_file_nonexistent() throws IOException {
     String nonexistentPath = temporaryFolder.newFolder().toPath().resolve("nonexistent").toString();
-    underTest.run(new String[]{"-source", nonexistentPath, "-target", newValidTargetPath()});
+    underTest.run(new String[] {"-source", nonexistentPath, "-target", newValidTargetPath()});
     verify(system2).printlnErr("Could not read configuration file: " + nonexistentPath + " (No such file or directory)");
     verify(system2).exit(1);
   }
@@ -141,7 +149,7 @@ public class MySQLMigratorTest {
   @Test
   public void exit_with_error_when_target_config_file_nonexistent() throws IOException {
     String nonexistentPath = temporaryFolder.newFolder().toPath().resolve("nonexistent").toString();
-    underTest.run(new String[]{"-source", newValidSourcePath(), "-target", nonexistentPath});
+    underTest.run(new String[] {"-source", newValidSourcePath(), "-target", nonexistentPath});
     verify(system2).printlnErr("Could not read configuration file: " + nonexistentPath + " (No such file or directory)");
     verify(system2).exit(1);
   }
@@ -149,7 +157,7 @@ public class MySQLMigratorTest {
   @Test
   public void exit_with_error_when_source_jdbc_url_missing() throws IOException {
     String path = temporaryFolder.newFile().toString();
-    underTest.run(new String[]{"-source", path, "-target", newValidTargetPath()});
+    underTest.run(new String[] {"-source", path, "-target", newValidTargetPath()});
     verify(system2).printlnErr("Missing required configuration: sonar.jdbc.url");
     verify(system2).exit(1);
   }
@@ -157,7 +165,7 @@ public class MySQLMigratorTest {
   @Test
   public void exit_with_error_when_target_jdbc_url_missing() throws IOException {
     String path = temporaryFolder.newFile().toString();
-    underTest.run(new String[]{"-source", newValidSourcePath(), "-target", path});
+    underTest.run(new String[] {"-source", newValidSourcePath(), "-target", path});
     verify(system2).printlnErr("Missing required configuration: sonar.jdbc.url");
     verify(system2).exit(1);
   }
@@ -165,7 +173,7 @@ public class MySQLMigratorTest {
   @Test
   public void exit_with_error_when_source_url_doesnt_start_with_jdbc() throws IOException {
     String path = newConfigFileWithoutCredentials("foo");
-    underTest.run(new String[]{"-source", path, "-target", newValidTargetPath()});
+    underTest.run(new String[] {"-source", path, "-target", newValidTargetPath()});
     verify(system2).printlnErr("Expected JDBC URL to start with 'jdbc:', got: foo");
     verify(system2).exit(1);
   }
@@ -173,7 +181,7 @@ public class MySQLMigratorTest {
   @Test
   public void exit_with_error_when_target_url_doesnt_start_with_jdbc() throws IOException {
     String path = newConfigFileWithoutCredentials("foo");
-    underTest.run(new String[]{"-source", newValidSourcePath(), "-target", path});
+    underTest.run(new String[] {"-source", newValidSourcePath(), "-target", path});
     verify(system2).printlnErr("Expected JDBC URL to start with 'jdbc:', got: foo");
     verify(system2).exit(1);
   }
@@ -181,7 +189,7 @@ public class MySQLMigratorTest {
   @Test
   public void exit_with_error_when_source_url_malformed() throws IOException {
     String path = newConfigFileWithoutCredentials("jdbc:");
-    underTest.run(new String[]{"-source", path, "-target", newValidTargetPath()});
+    underTest.run(new String[] {"-source", path, "-target", newValidTargetPath()});
     verify(system2).printlnErr("Expected JDBC URL in the form 'jdbc:driverClassName:url', got: jdbc:");
     verify(system2).exit(1);
   }
@@ -189,7 +197,7 @@ public class MySQLMigratorTest {
   @Test
   public void exit_with_error_when_target_url_malformed() throws IOException {
     String path = newConfigFileWithoutCredentials("jdbc:");
-    underTest.run(new String[]{"-source", newValidSourcePath(), "-target", path});
+    underTest.run(new String[] {"-source", newValidSourcePath(), "-target", path});
     verify(system2).printlnErr("Expected JDBC URL in the form 'jdbc:driverClassName:url', got: jdbc:");
     verify(system2).exit(1);
   }
@@ -197,7 +205,7 @@ public class MySQLMigratorTest {
   @Test
   public void exit_with_error_when_source_driver_unsupported() throws IOException {
     String path = newConfigFileWithoutCredentials("jdbc:foo:bar");
-    underTest.run(new String[]{"-source", path, "-target", newValidTargetPath()});
+    underTest.run(new String[] {"-source", path, "-target", newValidTargetPath()});
     verify(system2).printlnErr("Unsupported source driver: foo; supported drivers: mysql");
     verify(system2).exit(1);
   }
@@ -205,7 +213,7 @@ public class MySQLMigratorTest {
   @Test
   public void exit_with_error_when_target_driver_unsupported() throws IOException {
     String path = newConfigFileWithoutCredentials("jdbc:foo:bar");
-    underTest.run(new String[]{"-source", newValidSourcePath(), "-target", path});
+    underTest.run(new String[] {"-source", newValidSourcePath(), "-target", path});
     verify(system2).printlnErr("Unsupported target driver: foo; supported drivers: postgresql, oracle, sqlserver");
     verify(system2).exit(1);
   }
@@ -217,7 +225,7 @@ public class MySQLMigratorTest {
     String sourcePath = newConfigFileWithoutCredentials(sourceUrl);
     String targetUrl = "jdbc:" + driverName + ":bar";
     String targetPath = newConfigFileWithoutCredentials(targetUrl);
-    underTest.run(new String[]{"-source", sourcePath, "-target", targetPath});
+    underTest.run(new String[] {"-source", sourcePath, "-target", targetPath});
 
     ArgumentCaptor<ConnectionConfig> sourceArgumentCaptor = ArgumentCaptor.forClass(ConnectionConfig.class);
     ArgumentCaptor<ConnectionConfig> targetArgumentCaptor = ArgumentCaptor.forClass(ConnectionConfig.class);
@@ -243,7 +251,7 @@ public class MySQLMigratorTest {
     String sourcePath = newConfigFile(sourceUrl, "muser", "mpass");
     String targetUrl = "jdbc:" + driverName + ":bar";
     String targetPath = newConfigFile(targetUrl, "puser", "ppass");
-    underTest.run(new String[]{"-source", sourcePath, "-target", targetPath});
+    underTest.run(new String[] {"-source", sourcePath, "-target", targetPath});
 
     ArgumentCaptor<ConnectionConfig> sourceArgumentCaptor = ArgumentCaptor.forClass(ConnectionConfig.class);
     ArgumentCaptor<ConnectionConfig> targetArgumentCaptor = ArgumentCaptor.forClass(ConnectionConfig.class);
@@ -265,7 +273,7 @@ public class MySQLMigratorTest {
   @DataProvider
   public static Object[][] supportedTargetDriverNames() {
     return MySQLMigrator.SUPPORTED_TARGET_DRIVER_NAMES.stream()
-      .map(name -> new Object[]{name})
+      .map(name -> new Object[] {name})
       .toArray(Object[][]::new);
   }
 
@@ -274,7 +282,7 @@ public class MySQLMigratorTest {
     String message = "failed because...";
     doThrow(new RuntimeException(message)).when(mainExecutor).execute(same(system2), any(), any());
 
-    underTest.run(new String[]{"-source", newValidSourcePath(), "-target", newValidTargetPath()});
+    underTest.run(new String[] {"-source", newValidSourcePath(), "-target", newValidTargetPath()});
 
     verify(system2).printlnErr(message);
     verify(system2).exit(1);
@@ -288,18 +296,29 @@ public class MySQLMigratorTest {
       return null;
     }).when(mainExecutor).execute(same(system2), any(), any());
 
-    underTest.run(new String[]{"-source", newValidSourcePath(), "-target", newValidTargetPath()});
+    underTest.run(new String[] {"-source", newValidSourcePath(), "-target", newValidTargetPath()});
 
     verify(system2).printlnOut("some output");
     verify(system2).printlnErr("some error");
     verify(system2).exit(0);
   }
 
+  @Test
+  public void exit_with_error_when_real_execution_fails() throws Exception {
+    String sourceUrl = "jdbc:mysql:nonexistent";
+    String sourcePath = newConfigFileWithoutCredentials(sourceUrl);
+    String targetUrl = "jdbc:postgresql:nonexistent";
+    String targetPath = newConfigFileWithoutCredentials(targetUrl);
+
+    exit.expectSystemExitWithStatus(1);
+    realRun("-source", sourcePath, "-target", targetPath);
+  }
+
   private String newConfigFile(String url, String username, String password) throws IOException {
     Path targetPath = temporaryFolder.newFile().toPath();
     String content = String.format("sonar.jdbc.url = %s\n" +
-        "sonar.jdbc.username = %s\n" +
-        "sonar.jdbc.password = %s\n",
+      "sonar.jdbc.username = %s\n" +
+      "sonar.jdbc.password = %s\n",
       url, username, password);
     Files.write(targetPath, content.getBytes());
     return targetPath.toString();
@@ -318,5 +337,9 @@ public class MySQLMigratorTest {
 
   private String newValidTargetPath() throws IOException {
     return newConfigFileWithoutCredentials("jdbc:postgresql:foo");
+  }
+
+  private static void realRun(String... args) {
+    MySQLMigrator.main(args);
   }
 }
