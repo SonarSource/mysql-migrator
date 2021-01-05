912e0e6559191de2ceb25b1b479e0c0a03ba2fd2
Wouter Admiraal <wouter.admiraal@sonarsource.com>
2021-01-05 08:08:51 +0100

    Update license headers for 2021

--- src/test/java/org/sonarsource/sqdbmigrator/migrator/before/NonBlankSourceValidatorTest.java
+++ src/test/java/org/sonarsource/sqdbmigrator/migrator/before/NonBlankSourceValidatorTest.java
@@ -1,6 +1,6 @@
 /*
  * SonarQube MySQL Database Migrator
- * Copyright (C) 2019-2019 SonarSource SA
+ * Copyright (C) 2019-2021 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
